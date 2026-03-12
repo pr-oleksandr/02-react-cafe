96daa3674fb89358804e01643f1541a674013b5b
Саша Присташ <sashko@MacBook-Air-Sasa.local>
2026-03-12 17:49:17 +0200

    add a feedback stats markup

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import css from "./App.module.css"
+import type { Votes, VoteType } from "./types/votes";
+import CafeInfo from "./components/CafeInfo/CafeInfo"
+import VoteOptions from "./components/VoteOptions/VoteOptions";
+import VoteStats from "./components/VoteStats/VoteStats"
+import Notification from "./components/Notification/Notification"
+
+const initialVotes: Votes = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+};
+
+
+function App() {
+  const [votes, setVotes] = useState<Votes>(initialVotes);
+
+  const handleVote = (type: VoteType) => {
+    setVotes(prev => ({
+      ...prev,
+      [type]: prev[type] + 1
+    }));
+  };
+
+  const resetVotes = () => {
+    setVotes(initialVotes);
+  };
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+
+  return (
+    <div className={css.app}>
+      < CafeInfo />
+      < VoteOptions onVote={handleVote} onReset={resetVotes} canReset={totalVotes > 0} />
+      {totalVotes > 0 ? (< VoteStats votes={votes} totalVotes={totalVotes} positiveRate={totalVotes === 0 ? 0
+        : Math.round((votes.good / (totalVotes)) * 100)} />) : (< Notification />)}
+   </div>
+
+  )
+}
+
+export default App
+
